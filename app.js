efa3f5dd84860e0ad9ea2ddb0b3b1e1053a69afb
Vinay-Sriram-Tummidi <vinaysriram.tummidi@gmail.com>
2026-01-31 12:43:46 +0530

    added github actions WorkFlow

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Jenkins + Docker 🚀");
+  res.end("Hello from Jenkins + Docker!!! 🚀");
 });
 
 server.listen(3000, () => {
